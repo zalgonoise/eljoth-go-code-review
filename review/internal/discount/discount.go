cbee98fe51ecf6726c3ea53df5ecea7c10a2a49a
zalgonoise <zalgo.noise@gmail.com>
2023-02-15 14:18:02 +0100

    int/discount: added NewCoupon fn (with validation)

--- review/internal/discount/discount.go
+++ review/internal/discount/discount.go
@@ -1,5 +1,18 @@
 package discount
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidDiscount     = errors.New("invalid coupon discount value")
+	ErrInvalidCode         = errors.New("invalid coupon code")
+	ErrInvalidMinimumValue = errors.New("invalid coupon minimum basket value")
+)
+
 // Coupon describes the entity
 //
 // A coupon is used to apply a discount
@@ -9,6 +22,27 @@ type Coupon struct {
 	Code           string
 	Discount       int
 	MinBasketValue int
+}
+
+// NewCoupon creates a Coupon with validation
+func NewCoupon(discountVal int, code string, minBasketValue int) (*Coupon, error) {
+	if discountVal <= 0 {
+		return nil, fmt.Errorf("%w: cannot be %d", ErrInvalidDiscount, discountVal)
+	}
+	if code == "" {
+		return nil, fmt.Errorf("%w: empty string", ErrInvalidCode)
+	}
+	if minBasketValue < 0 {
+		return nil, fmt.Errorf("%w: cannot be below zero: %d", ErrInvalidMinimumValue, minBasketValue)
+	}
+
+	return &Coupon{
+		Discount:       discountVal,
+		Code:           code,
+		MinBasketValue: minBasketValue,
+		ID:             uuid.NewString(),
+	}, nil
+
 }
 
 // Basket describes the entity
